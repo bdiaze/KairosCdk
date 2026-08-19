e2eff38e3d1fb47645d5f2746dbe6013b627415f
Benjamín <benjamin.diaze@outlook.com>
2026-08-18 20:54:27 -0400

    Se integra API con Scalar.

--- ApiCalendarizarProcesos/ApiCalendarizarProcesos/Program.cs
+++ ApiCalendarizarProcesos/ApiCalendarizarProcesos/Program.cs
@@ -1,11 +1,15 @@
 using Amazon.DynamoDBv2;
 using Amazon.Lambda.Serialization.SystemTextJson;
 using Amazon.Scheduler;
+using ApiCalendarizarProcesos.Business;
 using ApiCalendarizarProcesos.Endpoints;
 using ApiCalendarizarProcesos.Helpers;
 using ApiCalendarizarProcesos.Interfaces.Helpers;
+using ApiCalendarizarProcesos.UseCases;
 using LibreriaCompartida.Helpers;
 using LibreriaCompartida.Interfaces.Helpers;
+using LibreriaCompartida.Repositories;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
@@ -16,9 +20,34 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi, new SourceGeneratorLambdaJsonSerializer<AppJsonSerializerContext>());
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi(c => {
+	c.AddDocumentTransformer((document, context, cancellationToken) => {
+		document.Info = new() {
+			Title = "API Kairos - Minimal API AoT",
+			Version = "v1"
+		};
+
+		return Task.CompletedTask;
+	});
+});
+
 #region Singleton AWS Services
-builder.Services.AddSingleton<IAmazonDynamoDB, AmazonDynamoDBClient>();
-builder.Services.AddSingleton<IAmazonScheduler, AmazonSchedulerClient>();
+builder.Services.AddSingleton<IAmazonDynamoDB>(sp => {
+	AmazonDynamoDBConfig config = new() {
+		ConnectTimeout = TimeSpan.FromSeconds(5),
+		Timeout = TimeSpan.FromSeconds(25)
+	};
+	return new AmazonDynamoDBClient(config);
+});
+builder.Services.AddSingleton<IAmazonScheduler>(sp => {
+	AmazonSchedulerConfig config = new() {
+		ConnectTimeout = TimeSpan.FromSeconds(5),
+		Timeout = TimeSpan.FromSeconds(25)
+	};
+
+	return new AmazonSchedulerClient(config);
+});
 #endregion
 
 #region Singleton Helpers
@@ -27,7 +56,26 @@ builder.Services.AddSingleton<ISchedulerHelper, SchedulerHelper>();
 builder.Services.AddSingleton<IDynamoHelper, DynamoHelper>();
 #endregion
 
-var app = builder.Build();
+#region Singleton Daos
+builder.Services.AddSingleton<ProcesoDao>();
+builder.Services.AddSingleton<CalendarizacionDao>();
+#endregion
+
+#region Singleton Business
+builder.Services.AddSingleton<CalendarizacionBusiness>();
+builder.Services.AddSingleton<ProcesoBusiness>();
+#endregion
+
+#region Singleton Use Cases
+builder.Services.AddSingleton<ProcesoUseCase>();
+#endregion
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment()) {
+	app.MapOpenApi();
+	app.MapScalarApiReference();
+}
 
 app.MapProcesosEndpoints();
 
